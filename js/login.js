c2c8b40a75e8063773994a30df0442ca21ff2c63
shravanbhoyar1 <shravanbhoyar3507>
2025-09-17 19:00:11 +0530

    modify the toggle feature

---
+++ js/login.js
@@ -0,0 +1,18 @@
+const passwordInput = document.getElementById("password");
+const toggleImg = document.getElementById("toggle-icon");
+const passwordDiv = document.getElementById("password-div");
+
+function toggleImgView() {
+    if (passwordInput.type == "password") {
+        passwordInput.type = "text";
+        toggleImg.src = '../login-img/closed.png';
+    }
+    else {
+        passwordInput.type = "password";
+        toggleImg.src = '../login-img/open.png';
+    }
+}
+
+const changeOutline = ()=>{
+    passwordDiv.style="outline: auto;";
+}
