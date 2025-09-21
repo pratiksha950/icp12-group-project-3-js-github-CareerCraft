8878012ec6a23df6315443da9aa69522b48c6611
siddhikharde <siddhikharde2005@gmail.com>
2025-09-22 00:48:39 +0530

    filter method added to level select option

--- js/course.js
+++ js/course.js
@@ -1,4 +1,4 @@
-let filterCard =() => {
+let searchCard =() => {
    const searchInput=document.getElementById("search-input").value.toLowerCase();
    const cards=document.querySelectorAll('.course-card');
    cards.forEach(card =>{
@@ -6,5 +6,12 @@ let filterCard =() => {
       const discrip =document.querySelector('.course-info p').innerText.toLowerCase();
       card.style.display=(title.includes(searchInput) || discrip.includes(searchInput)) ? "block" : "none";
    })
-   
+}
+
+let filterCards=() =>{
+    const value=document.getElementById("level").value;
+    const cards= document.querySelectorAll(".course-card");
+    cards.forEach(card=>{
+        card.style.display=(card.dataset.level===value || value==="All") ? "block" :"none";
+    })
 }
