2e2c2808249f726be1f29667d46d3575859df92d
siddhikharde <siddhikharde2005@gmail.com>
2025-09-22 01:54:20 +0530

    basic chnages added

--- js/course.js
+++ js/course.js
@@ -2,11 +2,11 @@ let searchCard =() => {
    const searchInput=document.getElementById("search-input").value.toLowerCase();
    const cards=document.querySelectorAll('.course-card');
    cards.forEach(card =>{
-      const title=document.querySelector('.course-info h2').innerText.toLowerCase();
-      const discrip =document.querySelector('.course-info p').innerText.toLowerCase();
+      const title=card.querySelector('.course-info h2').innerText.toLowerCase();
+      const discrip =card.querySelector('.course-info p').innerText.toLowerCase();
       card.style.display=(title.includes(searchInput) || discrip.includes(searchInput)) ? "block" : "none";
    })
-}
+};
 
 let filterCards=() =>{
     const value=document.getElementById("level").value;
