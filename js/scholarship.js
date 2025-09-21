8d232836e2b265b0f6895da8ef75abc24ea9f9fd
siddhikharde <siddhikharde2005@gmail.com>
2025-09-21 10:11:34 +0530

    basic changes added

--- js/scholarship.js
+++ js/scholarship.js
@@ -12,6 +12,7 @@ let searchScholarships = () => {
     cards.forEach(card=>{
         const title = card.querySelector('.scho-text').innerText.toLowerCase();
         const description = card.querySelector('.scho-info p').innerText.toLowerCase();
-        card.style.display = (title.includes(searchInput) || description.includes(searchInput)) ? "block" : "none";
+        const eligibility = card.querySelector('.eligibility li').innerText.toLowerCase();
+        card.style.display = (title.includes(searchInput) || description.includes(searchInput) || eligibility.includes(searchInput)) ? "block" : "none";
     });
 }
