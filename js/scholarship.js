4f1e43774cb6c80487cf33f7fb2da12f7fffcb40
pratiksha950 <salunkepratiksha950@gmail.com>
2025-09-22 22:19:41 +0530

    scholer section added

--- js/scholarship.js
+++ js/scholarship.js
@@ -28,12 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
-
-
-
-
 function filterCards() {
     const value=document.getElementById('categories').value;
 const cards= document.querySelectorAll('.scho-card');
