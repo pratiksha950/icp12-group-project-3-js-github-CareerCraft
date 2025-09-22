5c1868048c1c6b4c4a02a03a3f08e3ef2f28950b
pratiksha950 <salunkepratiksha950@gmail.com>
2025-09-22 21:01:21 +0530

    career page added

--- js/career.js
+++ js/career.js
@@ -1,3 +1,36 @@
+ let path = location.pathname.includes("/pages/") ? "../pages/header.html" : "./pages/header.html";
+fetch(path)
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  })
+  .catch(err => console.error("Header load error:", err));
+
+  function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    if(sidebar){
+        sidebar.style.display = "flex";
+    }
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    if(sidebar){
+        sidebar.style.display = "none";
+    }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("../pages/footer.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("footer").innerHTML = data)
+    .catch(err => console.error("Footer load error:", err));
+});
+
+
+ 
+ 
+ 
+ 
  let index = 0;
     const slides = document.getElementById("slides");
     const totalSlides = document.querySelectorAll(".slide").length;
